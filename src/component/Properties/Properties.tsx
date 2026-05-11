d601e063efd0ea4b4c516bc431489a2de49b0d89
dofiki <dixitregmiofficial@gmail.com>
2026-05-11 21:20:37 +0545

    Added editable input in properties component

--- src/component/Properties/Properties.tsx
+++ src/component/Properties/Properties.tsx
@@ -1,24 +1,9 @@
+import { useEditorStore } from "../../store/useEditorStore";
 import type { Element } from "../../types";
 
 const Properties = ({ element }: { element: Element | null }) => {
+  const { updateElement } = useEditorStore();
   if (!element) return null;
-
-  const rows = [
-    ["id", element.id.slice(0, 8) + "..."],
-    ["x", element.x],
-    ["y", element.y],
-    ["width", element.width],
-    ["height", element.height],
-    ...(element.type === "text"
-      ? [
-          ["content", element.content ?? "—"],
-          ["fontSize", element.fontSize],
-          ["fontType", element.fontType],
-          ["color", element.textColor],
-        ]
-      : [["src", element.src.slice(0, 16) + "..."]]),
-  ];
-
   return (
     <div
       className="h-full w-full bg-[#002322] border 
@@ -33,17 +18,109 @@ const Properties = ({ element }: { element: Element | null }) => {
         </span>
       </div>
 
-      <div className="p-3 flex flex-col gap-1.5 font-mono">
-        {rows.map(([key, val]) => (
-          <div key={String(key)} className="flex justify-between gap-2">
-            <span className="text-[0.65rem] text-[#009b6a] uppercase tracking-wider">
-              {key}
-            </span>
-            <span className="text-[0.7rem] text-gray-300 truncate max-w-28">
-              {String(val)}
-            </span>
-          </div>
-        ))}
+      <div className="flex flex-col gap-2 mt-5 ">
+        <div className="text-white flex items-center justify-between pl-4 pr-2">
+          <label>X :</label>
+          <input
+            type="number"
+            className="text-white bg-[#022e2c] p-2 rounded-md "
+            value={element.x.toFixed()}
+            onChange={(e) =>
+              updateElement(element.id, { x: Number(e.currentTarget.value) })
+            }
+          />
+        </div>
+
+        <div className="text-white flex items-center justify-between pl-4 pr-2">
+          <label>Y :</label>
+          <input
+            type="number"
+            value={element.y.toFixed()}
+            className="text-white bg-[#022e2c] p-2 rounded-md"
+            onChange={(e) =>
+              updateElement(element.id, { y: Number(e.currentTarget.value) })
+            }
+          />
+        </div>
+        <div className="text-white flex items-center justify-between pl-4 pr-2">
+          <label>Height :</label>
+          <input
+            type="number"
+            value={element.height.toFixed()}
+            className="text-white bg-[#022e2c] p-2 rounded-md"
+            onChange={(e) =>
+              updateElement(element.id, {
+                height: Number(e.currentTarget.value),
+              })
+            }
+          />
+        </div>
+        <div className="text-white flex items-center justify-between pl-4 pr-2">
+          <label>Width :</label>
+          <input
+            type="number"
+            value={element.width.toFixed()}
+            className="text-white bg-[#022e2c] p-2 rounded-md"
+            onChange={(e) =>
+              updateElement(element.id, {
+                width: Number(e.currentTarget.value),
+              })
+            }
+          />
+        </div>
+
+        {element.type === "text" ? (
+          <>
+            {" "}
+            <div className="text-white flex items-center justify-between pl-4 pr-2">
+              <label>Content :</label>
+              <input
+                type="string"
+                value={element.content || ""}
+                className="text-white bg-[#022e2c] p-2 rounded-md "
+                onChange={(e) =>
+                  updateElement(element.id, { content: e.currentTarget.value })
+                }
+              />
+            </div>
+            <div className="text-white flex items-center justify-between pl-4 pr-2">
+              <label>Font Size :</label>
+              <input
+                type="number"
+                value={element.fontSize}
+                className="text-white bg-[#022e2c] p-2 rounded-md "
+                onChange={(e) =>
+                  updateElement(element.id, {
+                    fontSize: Number(e.currentTarget.value),
+                  })
+                }
+              />
+            </div>
+            <div className="text-white flex items-center justify-between pl-4 pr-2">
+              <label>Font :</label>
+              <input
+                type="string"
+                value={element.fontType}
+                className="text-white bg-[#022e2c] p-2 rounded-md cursor-not-allowed"
+              />
+            </div>
+            <div className="text-white flex items-center justify-between pl-4 pr-2">
+              <label>Color :</label>
+              <input
+                type="string"
+                value={element.textColor}
+                className="text-white bg-[#022e2c] p-2 rounded-md"
+                onChange={(e) =>
+                  updateElement(element.id, {
+                    textColor: e.currentTarget.value,
+                  })
+                }
+              />
+            </div>
+          </>
+        ) : (
+          " "
+        )}
       </div>
     </div>
   );
